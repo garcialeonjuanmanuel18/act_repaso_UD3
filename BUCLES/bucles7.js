dd57085420ba6dde7965bd5ef53e2a8b2083f965
garcialeonjuanmanuel18@gmail.com <garcialeonjuanmanuel18@gmail.com>
2026-03-14 14:26:45 +0100

    act 7 hecha

--- BUCLES/bucles7.js
+++ BUCLES/bucles7.js
@@ -3,9 +3,13 @@
 
 let array = [0, 1, 2, 3, 4];
 let n = 1;
+let ultimo = array[array.length-n];
 
-for(let i = 0; i < array.length; i++){
-    array[i]=array[i-1];
+for(let i = array.length-n; i > 0; i--){
+   array[i]=array[i-n];
+
 }
+
+array[0]=ultimo;
 
 console.log(array);
